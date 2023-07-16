a6fbe01fd66e5ecddd1a212908427d75a91a9d89
Md Asif Alam Alam <measifalam@gmail.com>
2023-07-16 15:33:26 +0530

    file and folder str

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Switch } from "react-router-dom";
 
-const App = () => (
-  
-    <div>App</div>
-  
-)
+import {Actors,MovieInformation,Movies,NavBar,Profile} from './componenets/index'
 
-export default App
+import useStyles from './componenets/styles'
+const App = () => {
+    const classes = useStyles()
+    return (
+        <div className={classes.root}>
+    <CssBaseline />
+    <NavBar/>
+    <main className={classes.content}>
+        <div className={classes.toolbar}/>
+      <Switch>
+        <Route exact path="/movies/:id">
+          <h1>
+            <MovieInformation/>
+          </h1>
+        </Route>
+        <Route exact  path="/actors/:id">
+          <h1>
+            <Actors/>
+          </h1>
+        </Route>
+        <Route exact path="/">
+          <h1>
+            <Movies/>
+          </h1>
+        </Route>
+        <Route exact path="/profile/:id">
+          <h1>
+            <Profile/>
+          </h1>
+        </Route>
+      </Switch>
+    </main>
+  </div>
+    )
+
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+ReactDOM.render(
+<BrowserRouter>
+<App/>
+</BrowserRouter>
 
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+, document.getElementById('root' ) );
